ed4b8d4bd4b2bebc6d3bb8dba4531f2010c2472a
Max <maxbl4@gmail.com>
2020-01-06 20:16:26 +0300

    Update packages, remove backendless

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
-import Backendless from 'backendless';
 
 @Injectable()
 export class DataService {
-  async loadCheckpoints(): Promise<string> {
-    var whereClause = "name = 'checkpoints'";
-    var queryBuilder = Backendless.DataQueryBuilder.create().setWhereClause( whereClause );
-    const found: any = await Backendless.Data.of('TestTable').findFirst(queryBuilder);
-    console.log(found);
-    if (found) return found.data;
-    return null;
+  loadCheckpoints() {
+    return localStorage.getItem('checkpoints');
   }
 
-  async saveCheckpoints(data: string): Promise<object> {
-    return Backendless.Data.of('TestTable').save({ name: 'checkpoints', data: data })
+  saveCheckpoints(data: string) {
+    return localStorage.setItem('checkpoints', data);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import Backendless from 'backendless';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-
-const APP_ID: string = '5FED38D0-4FEA-0BE1-FF61-037A9C4E2E00';
-const API_KEY: string = 'BDD54FFF-E75F-3884-FF24-A72A8BDD0F00';
-Backendless.serverURL = 'https://api.backendless.com';
-Backendless.initApp(APP_ID, API_KEY);
 
 if (environment.production) {
   enableProdMode();
